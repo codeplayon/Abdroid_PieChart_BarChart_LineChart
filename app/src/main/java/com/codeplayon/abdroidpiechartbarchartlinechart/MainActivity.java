a25961ba1e543f791ff132bab141ceb0e0e7d7ee
codeplayon <codeplayon@gmail.com>
2021-04-04 02:50:35 -0400

    Button For chart example

--- app/src/main/java/com/codeplayon/abdroidpiechartbarchartlinechart/MainActivity.java
+++ app/src/main/java/com/codeplayon/abdroidpiechartbarchartlinechart/MainActivity.java
@@ -3,12 +3,45 @@ package com.codeplayon.abdroidpiechartbarchartlinechart;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button SimpleBar , StackedBar, GroupBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FindViewById();
+    }
+
+    public void FindViewById(){
+        SimpleBar=findViewById(R.id.SimpleBar);
+        StackedBar=findViewById(R.id.StackedBar);
+        GroupBar=findViewById(R.id.GroupBar);
+
+        SimpleBar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        StackedBar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        GroupBar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
+
